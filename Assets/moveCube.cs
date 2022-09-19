7c5a8b8e498312578048ef3c927a3c1bc018948c
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-09-19 08:04:01 +0700

    Now moving in 3D. And 4 degrees of freedom! Yaw included.

--- Assets/moveCube.cs
+++ Assets/moveCube.cs
@@ -5,33 +5,51 @@ using UnityEngine;
 public class moveCube : MonoBehaviour
 {
     float speed = 10.0f;
+    float turnSpeed = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Successful started Unity Impact 2022 at SSIS.");        
+        Debug.Log("Successful started Unity Impact 2022 at SSIS. Fixed for GITHUB.");        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up")) //     forward
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
-        if (Input.GetKey("down"))
+        if (Input.GetKey("down")) //   back
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
-        if (Input.GetKey("left"))
+        if (Input.GetKey("left")) //  <-
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-        if (Input.GetKey("right"))
+        if (Input.GetKey("right")) //  ->
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }        
-        if (Input.GetKeyDown("space"))
+        }
+        if (Input.GetKey(KeyCode.W)) // W
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) // S
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        // from video https://youtu.be/1-uLP7GGcRc?t=478
+        if (Input.GetKey(KeyCode.D)) // rotate left
+        {
+            transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.A)) // rotate right
+        {
+            transform.Rotate(0f, -turnSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey("space")) //  just print present coordinates to console
         {
             Debug.Log("Speed is " + speed + transform.position);
         }
